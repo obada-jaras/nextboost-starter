856c60676d25165d2da796f82775ea597dc914e4
Obada Jaras <obada.jaras@yahoo.com>
2025-01-27 01:15:16 +0200

    feat: add language switcher and locale handling with cookies
    - Added buttons to switch between Arabic and English.
    - Implemented locale management using cookies.
    - Created `locale` service for getting and setting user locale.
    - Updated translations for language switcher.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,19 @@
 
 import { useTheme } from "next-themes";
 import { useTranslations } from "next-intl";
+import { setUserLocale } from "@/services/locale";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
   const t = useTranslations("Sample");
+
+  const switchToArabic = async () => {
+    await setUserLocale("ar");
+  };
+
+  const switchToEnglish = async () => {
+    await setUserLocale("en");
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8 bg-[var(--color-background)] text-[var(--color-foreground)]">
@@ -16,6 +25,20 @@ export default function Home() {
       >
         {theme === "light" ? t("switchToDark") : t("switchToLight")}
       </button>
+      <div className="mt-5 flex gap-4">
+        <button
+          className="px-4 py-2 rounded bg-foreground text-background transition hover:bg-opacity-80 dark:hover:bg-opacity-70"
+          onClick={switchToArabic}
+        >
+          {t("switchToArabic")}
+        </button>
+        <button
+          className="px-4 py-2 rounded bg-foreground text-background transition hover:bg-opacity-80 dark:hover:bg-opacity-70"
+          onClick={switchToEnglish}
+        >
+          {t("switchToEnglish")}
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/i18n/config.ts
@@ -0,0 +1,4 @@
+type locales = ["ar", "en"];
+export type Locale = locales[number];
+
+export const defaultLocale: Locale = "ar";
